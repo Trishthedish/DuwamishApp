b38347bd672033e2a9acc94da00b490e1b274d16
brayzen <brian.ray.biz@gmail.com>
2016-01-19 16:07:43 -0800

    updated users to BACKERS, form submiting and updating firebase

--- public/js/takeAStand/tas.js
+++ public/js/takeAStand/tas.js
@@ -1,42 +1,56 @@
 $(document).ready(function(){
-    // read and dynamically insert total signatures from Firebase, currently reading total
-    //   number of users, which will likely change to 'backers' or something similar
-    //   when the petition actually begins.
-    var userCount = new Firebase('https://standwithduwamish.firebaseio.com/userCount');
-    userCount.on("value", function(snapshot){
-      var numSigs = snapshot.val();
-      $('#signatures').html(numSigs); // set initial value of users/signatures
-      localStorage.userCount = numSigs;
-      $('#sigs-needed').html(100000 - numSigs);
+  // read and dynamically insert total signatures from Firebase, currently reading total
+  //   number of backers
+  var backerCount = new Firebase('https://standwithduwamish.firebaseio.com/backerCount');
+  backerCount.on("value", function(snapshot){
+    var numSigs = snapshot.val();
+    $('#signatures').html(numSigs); // set initial value of users/signatures
+    localStorage.backerCount = numSigs;
+    $('#sigs-needed').html(100000 - numSigs);
+  });
 
-    })
+  // will count users in database and update backerCount....to be done after every user
+  // submits to signing petition....they are now 'backers'
+  function updateBackers(backerCount){
+    var sig = new Firebase('https://standwithduwamish.firebaseio.com/backers');
+    sig.on("value", function(snapshot) {
+      var numSigs = snapshot.numChildren();
+      backerCount.set(numSigs);
+      localStorage.backerCount = numSigs; //
+    });
+    showProgress(localStorage.backerCount);  //using local storage for the backer count
+  };
 
-    // will count users in database and update userCount....to be done after every user
-    // submits to signing petition
-    function setUserCount(userCount){
-      var sig = new Firebase('https://standwithduwamish.firebaseio.com/users');
-      sig.on("value", function(snapshot) {
-        var numSigs = snapshot.numChildren();
-        userCount.set(numSigs);
-        localStorage.userCount = numSigs; //
-      });
-      showProgress(localStorage.userCount);
-    };
+  // progress bar, will register 10% for visual sake until the signature total is higher
+  function showProgress(signatures){
+    $(".meter > span").each(function() {
+      var percent = (signatures * 100)/100000;
+      var adjPercent = percent > 10 ? percent : 10; // 10% minimum showing
+      $(this)
+        .data("origWidth",  adjPercent + "%")
+        .width(0)
+        .animate({
+          width: $(this).data('origWidth') // or + "%" if fluid
+        }, 1200);
+    });
+  };
 
-    // progress bar, will register 10% for visual sake until the signature total is higher
-    function showProgress(signatures){
-      $(".meter > span").each(function() {
-        var percent = (signatures * 100)/100000;
-        var adjPercent = percent > 10 ? percent : 10; // 10% minimum showing
-        $(this)
-          .data("origWidth",  adjPercent + "%")
-          .width(0)
-          .animate({
-            width: $(this).data('origWidth') // or + "%" if fluid
-          }, 1200);
-      });
-    };
-
-  setUserCount(userCount);
+  //listen for submit to update backer in firebase
+  $('button#pledge').on('click', function(event){
+    event.preventDefault();
+    var firstName = $('#firstName').val()
+    $('.action-call').hide();
+    $('.thanx-message').show();
+    $('#insert-name').html(firstName)
+    var backer = new Firebase('https://standwithduwamish.firebaseio.com/backers');
+    backer.set({backer: {
+      first: firstName,
+      last: $('#lastName').val(),
+      email: $('#email').val(),
+      zipcode: $('#zip').val(),
+    }});
+    updateBackers(backerCount);
+  })
+  updateBackers(backerCount);
 })
 
