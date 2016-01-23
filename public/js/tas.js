9d1fc6367fa6232f72bcbd200ae8a9a6750bade1
brayzen <brian.ray.biz@gmail.com>
2016-01-23 12:10:49 -0800

    add class .signed and maskForm() to overlay once submit is pressed

--- public/js/tas.js
+++ public/js/tas.js
@@ -1,4 +1,9 @@
 $(function(){
+  var signed = localStorage.signed;
+  if (signed === "true"){
+    maskForm(localStorage.firstName);
+  }
+
   // read and dynamically insert total signatures from Firebase, currently reading total
   //   number of backers
   var backerCount = new Firebase('https://standwithduwamish.firebaseio.com/backerCount');
@@ -9,10 +14,15 @@ $(function(){
     $('#sigs-needed').text(100000 - signatureCount);
   });
 
+  function maskForm(firstName){
+    console.log('ran maskForm');
+    $('.step1').fadeOut(500).empty().html('<h3>Thank you ' + firstName + ' for supporting us</h3>').addClass('signed').fadeIn(1000);
+  }
+
   // will count users in database and update backerCount....to be done after every user
   // submits to signing petition....they are now 'backers'
-  var backers = new Firebase('https://standwithduwamish.firebaseio.com/backers');
   function updateBackers(backerCount){
+    var backers = new Firebase('https://standwithduwamish.firebaseio.com/backers');
     backers.on("value", function(snapshot) {
       var backerTotal = snapshot.numChildren();
       backerCount.set(backerTotal); // updates firebase backerCount (a smaller query than always counting the thousands of backers for an intial load/ note lines 4 - 10)
@@ -40,28 +50,32 @@ $(function(){
     return re.test(email);
   }
 
+  // http://stackoverflow.com/questions/1026069/capitalize-the-first-letter-of-string-in-javascript
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   //listen for submit to update backer in firebase
   // should also make api call to change.org *************(NOT YET)************
 
   $('button#pledge').on('click', function(event){
     event.preventDefault();
-    var firstName = $('#firstName').val();
+    var firstName = capitalizeFirstLetter($('#firstName').val());
     var email = $('#email').val();
     var zip = $('#zip').val();
+    var lastName = $('#lastName').val();
     if (zip.length === 5 && validateEmail(email)){
-      // $('.action-call').hide(); // hide call-to-action form
-      $('.thanx-message div').show(); // show hidden html
-      $('#insert-name').text(firstName);
-      $('.step1').hide();
       var backer = new Firebase('https://standwithduwamish.firebaseio.com/backers/');
       backer.push({
         first: firstName,
-        last: $('#lastName').val(),
+        last: lastName,
         email: email,
         zipcode: zip,
       });
+      maskForm(firstName);
       updateBackers(backerCount);
     } else {
+      console.log('Error with form');
       $('#error').show().text("Check your email address and/or your zipcode for errors");
     }
   });
