52760d1bbbcede71f3f18e319ed45789eec94e68
Andy Ernst <AndyCErnst@gmail.com>
2016-01-24 21:19:47 -0800

    sort out duplicate color variable bug, add responsive nav menu

--- public/js/app.js
+++ public/js/app.js
@@ -17,4 +17,20 @@ $(function() {
   // Add twitter widget functionality
   !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
   // jshint ignore:end
+
+  $('body').on('click', function(e) {
+     $('.nav-links-container').removeClass('expanded');
+     $('#menu-icon').attr('aria-expanded', false);
+  });
+  $('#menu-icon').on('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var $this = $(this);
+    if($this.attr('aria-expanded') === 'true') {
+      $this.attr('aria-expanded', false);
+    } else {
+      $this.attr('aria-expanded', true);
+    }
+    $('.nav-links-container').toggleClass('expanded');
+  });
 });
