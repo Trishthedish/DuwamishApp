91096b90a7bd57a9c8f51cc181cd30369093d183
Andy Ernst <andernst@gmail.com>
2016-01-26 19:57:39 -0800

    create ajax endpoint, add sha hashing package

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var sha256 = require('sha.js')('sha256');
 
+/*-- Static file server --*/
 app.use(express.static('dist'));
 
 var server = app.listen(process.env.PORT || 5000, function() {
@@ -8,5 +10,16 @@ var server = app.listen(process.env.PORT || 5000, function() {
   console.log('serving files from ' +  __dirname + '/dist/');
 })
 
+/*-- AJAX end points --*/
+var secretKey = process.env.CHANGE_ORG_KEY || '';
+var hashing = sha256.update(secretKey, 'utf8');
 
-
+app.get('/ajax/change-petition', function(req, res){
+	var obj = {};
+	console.log('received this query');
+	console.log(req.query);
+	res.send({
+		query: req.query,
+		hash: hashing.digest('hex')
+	});
+});
