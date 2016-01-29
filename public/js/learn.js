bb0441e83180f47601fafd8f7aee91505c39b8a2
Andy Ernst <AndyCErnst@gmail.com>
2016-01-28 21:30:22 -0800

    start tweaking timeline

---
+++ public/js/learn.js
@@ -0,0 +1,4 @@
+// var timelineDOM = frames.timeline;
+// var css = '<style>body: background-color: red !important;</style>';
+// $(timelineDOM.document.body).append(css);
+// '<link rel="stylesheet" type="text/css" href="css/style.css"
