bd7f05973be78e6bb8d5320334a1eab7c191aaff
Andy Ernst <AndyCErnst@gmail.com>
2016-02-21 14:46:23 -0800

    fix input selection bug, remove unneeded css classes

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,7 @@
 $(function() {
+  if(!$('.index').length) {
+    return; // Only apply on landing pages.
+  }
   // jshint ignore:start
   // Add twitter widget functionality
   !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
